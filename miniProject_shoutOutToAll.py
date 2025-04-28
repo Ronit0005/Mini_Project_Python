da6c81ea0ea7728e48093952a48a58525b0d5409
Ronti0005 <rontisingh24012007@gmail.com>
2025-04-28 19:12:05 +0530

    Editing the mini project VoiceOver

--- miniProject_shoutOutToAll.py
+++ miniProject_shoutOutToAll.py
@@ -1,6 +1,6 @@
 import win32com.client 
 speaker = win32com.client.Dispatch("SAPI.SpVoice") 
-l=['Ronit','Rishi','Rahul','Ritik','Abhishek','Sakshi','tanisha']
-nl=list(map(lambda x:'shout out to '+x,l))
+l=['light','camera','fashion']
+nl=list(map(lambda x:x,l))
 for s in nl:
   speaker.Speak(s) 
